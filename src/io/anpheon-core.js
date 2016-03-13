19a9fc23580b839d33c7cdece52233f9d9c66fed
Joel Morrison <spinbitz@gmail.com>
2016-03-12 22:05:16 -0700

    stylings

--- src/io/anpheon-core.js
+++ src/io/anpheon-core.js
@@ -1,23 +1,23 @@
-//import Cycle from '@cycle/core';
-//import {makeDOMDriver, div, input, p} from '@cycle/dom';
-//
-//function main(drivers) {
-//    return {
-//        DOM: drivers.DOM.select('canvas').events('click')
-//            .map(ev => {
-//                    "use strict";
-//                    console.log("ev is ... ", ev);
-//                    div('#canvas-id', [
-//                        canvas("some canvas?")
-//                    ])
-//                }
-//            )
-//            .startWith({bob: "dobbs"})
-//    };
-//}
-//
-//const drivers = {
-//    DOM: makeDOMDriver('#topnav-hero')
-//};
-//
-//Cycle.run(main, drivers);
+import Cycle from '@cycle/core';
+import {makeDOMDriver, div, input, p} from '@cycle/dom';
+
+function main(drivers) {
+    return {
+        DOM: drivers.DOM.select('#hero-canvas').events('click')
+            .map(ev => {
+                    "use strict";
+                    console.log("ev is ... ", ev);
+                    div('#canvas-id', [
+                        canvas("some canvas?")
+                    ])
+                }
+            )
+            .startWith({bob: "dobbs"})
+    };
+}
+
+const drivers = {
+    DOM: makeDOMDriver('#topnav-hero')
+};
+
+Cycle.run(main, drivers);
